24e5f9c8c76a78a486ca005d5cb49517553525c2
Patrick <patrick.obaf@gmail.com>
2019-10-31 12:44:00 +0100

    First repo

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Freeswitch\FreeswitchESL;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $freeswitch = new FreeswitchESL();
+        $connect = $freeswitch->connect("127.0.0.1","8021","ClueCon");
+        if ($connect) {
+            dd('success');
+        }
+    }
+}
---
+++ config/freeswitch.php
@@ -0,0 +1,19 @@
+<?php
+
+return  [
+
+
+
+
+    'freeswitch' => [
+        'hostname' => env('FREESWITCH_HOST'),
+        'port' => env('FREESWITCH_PORT'),
+        'password' => env('FREESWITCH_PASSWORD')
+    ],
+
+
+
+
+];
+
+
